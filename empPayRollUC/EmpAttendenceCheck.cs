0d876480da15cf21e2c73e30d255f058230f8c21
LokeshKiran <lokeshkskiran@gmail.com>
2022-08-25 22:11:47 +0530

    [LokeshKiran]Added:Employee is present or absent check

---
+++ empPayRollUC/EmpAttendenceCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empPayRollUC
+{
+    internal class EmpAttendenceCheck
+    {
+        public static void EmpAtt()
+        {
+            Random random = new Random();
+            int empAtt = random.Next(2);
+            if(empAtt == 0)
+                Console.WriteLine("employee is absent");
+            else
+                Console.WriteLine("employee is present");
+        }
+    }
+}
